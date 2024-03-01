0ba273f1971318790025c4cb79d063c549c5d9b6
ryanbax17 <ryan.bax.17@gmail.com>
2024-02-29 19:48:14 -0500

    Update to tab1 to include time picker

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,6 +6,140 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  public pickerColumns = [
+    {
+      name: 'hours',
+      options: [
+        { 
+          text: '0',
+          value: 0,
+        },
+        { 
+          text: '1',
+          value: 1,
+        },
+        { 
+          text: '2',
+          value: 2,
+        },
+        { 
+          text: '3',
+          value: 3,
+        },
+      ],
+    },
+    {
+      name: 'minutes',
+      options: [
+        { 
+          text: '0',
+          value: 0,
+        },
+        { 
+          text: '1',
+          value: 1,
+        },
+        { 
+          text: '2',
+          value: 2,
+        },
+        { 
+          text: '3',
+          value: 3,
+        },
+        { 
+          text: '4',
+          value: 4,
+        },
+        { 
+          text: '5',
+          value: 5,
+        },
+        { 
+          text: '6',
+          value: 6,
+        },
+        { 
+          text: '7',
+          value: 7,
+        },
+        { 
+          text: '8',
+          value: 8,
+        },
+        { 
+          text: '9',
+          value: 9,
+        },
+        { 
+          text: '10',
+          value: 10,
+        },
+      ],
+    },
+    {
+      name: 'seconds',
+      options: [
+        { 
+          text: '0',
+          value: 0,
+        },
+        { 
+          text: '1',
+          value: 1,
+        },
+        { 
+          text: '2',
+          value: 2,
+        },
+        { 
+          text: '3',
+          value: 3,
+        },
+        { 
+          text: '4',
+          value: 4,
+        },
+        { 
+          text: '5',
+          value: 5,
+        },
+        { 
+          text: '6',
+          value: 6,
+        },
+        { 
+          text: '7',
+          value: 7,
+        },
+        { 
+          text: '8',
+          value: 8,
+        },
+        { 
+          text: '9',
+          value: 9,
+        },
+        { 
+          text: '10',
+          value: 10,
+        },
+      ],
+    },
+  ];
+
+  public pickerButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+    },
+    {
+      text: 'Confirm',
+      handler: (value: any) => {
+        console.log('Selected:', value.hours.text,'h', value.minutes.text,'m', value.seconds.text,'s');
+      },
+    },
+  ];
 
   constructor() {}
 
