1a8459631df3c3b87f61a82021e1a3053d3b3a3e
Ryan Baxter <ryan.bax.17@gmail.com>
2024-03-07 20:12:49 -0500

    Functional time countdown
    Tab1.page now shows the hours, minutes, and seconds counting down and update correctly. In addition, a Start and Cancel button have been added.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,6 +11,9 @@ export class Tab1Page {
   private stop$ = new Subject<void>();
   isRunning = false;
   timeLeft = 0;
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
   interval$ = interval(1000);
 
   constructor() { }
@@ -18,18 +21,31 @@ export class Tab1Page {
   startTimer(): void {
     this.interval$.pipe(takeUntil(this.stop$)).subscribe(() => {
       if (this.timeLeft > 0) {
+        console.log('tick')
         this.timeLeft--;
+        this.hours = Math.floor(this.timeLeft / 3600);
+        this.minutes = Math.floor(this.timeLeft % 3600 / 60);
+        this.seconds = Math.floor(this.timeLeft % 60);
       } else {
+        console.log('tock')
         this.finish();
       }
     });
   }
 
+  cancelTimer(): void {
+    this.timeLeft = 0;
+    this.hours = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+    this.finish();
+  }
+
   finish(): void {
     this.isRunning = false;
+    console.log('Timer is finished!');
     this.stop$.next();    
     this.stop$.complete();
-    console.log('Timer is finished!');    
   }
 
   populateColumnOptions(count: number) {
@@ -49,9 +65,10 @@ export class Tab1Page {
       text: 'Confirm',
       handler: (value: any) => {
         this.timeLeft = value.hours.value * 3600 + value.minutes.value * 60 + value.seconds.value;
-        console.log('Time in seconds: ', this.timeLeft)
-        this.startTimer();
-        this.isRunning = true;
+        this.hours = Math.floor(this.timeLeft / 3600);
+        this.minutes = Math.floor(this.timeLeft % 3600 / 60);
+        this.seconds = Math.floor(this.timeLeft % 60);
+        this.isRunning = true;       
       },
     },
   ];
